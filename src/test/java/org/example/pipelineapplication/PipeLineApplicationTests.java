e8da5fc0e96b331071f95210e7be132368653198
유시환 <tlghks1217@5finger.co.kr>
2025-08-11 21:29:13 +0900

    [init] application initialize

---
+++ src/test/java/org/example/pipelineapplication/PipeLineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pipelineapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipeLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
