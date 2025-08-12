c861c2ccc0e870c4432a304f545089b9ffecbbb9
유시환 <tlghks1217@5finger.co.kr>
2025-08-12 22:07:34 +0900

    [feat] webhook 설정 관ë 테스트 로그 추가

--- src/main/java/org/example/pipelineapplication/simple/controller/SimpleController.java
+++ src/main/java/org/example/pipelineapplication/simple/controller/SimpleController.java
@@ -1,6 +1,7 @@
 package org.example.pipelineapplication.simple.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
+@Slf4j
 public class SimpleController {
 
     @GetMapping("/getSentence")
     public ResponseEntity<String> getSentence() {
+        log.info("github Webhook Test By ngrok");
         return ResponseEntity.status(HttpStatus.OK).body("Hello, World");
     }
 }
